df84453640cc4aa414f8dd11706959406da02e9a
JackLian <jack.lianjie@gmail.com>
2012-08-14 17:47:33 +0800

    initCommit

---
+++ src/gen/com/itotem/jacktestapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.itotem.jacktestapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/src/com/itotem/jacktestapp/DisplayMessageActivity.java
@@ -0,0 +1,28 @@
+package com.itotem.jacktestapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// setContentView(R.layout.activity_display_message);
+		// getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		Intent intent = getIntent();
+		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+		// Create the text view
+		TextView textView = new TextView(this);
+		textView.setTextSize(40);
+		textView.setText(message);
+
+		// Set the text view as the activity layout
+		setContentView(textView);
+	}
+
+}
